2f1c9f91c36644ff3c7fc116c6bab3dcc9169548
minhthanh1703 <minhthanh1703>
2020-09-18 12:56:01 +0700

    thanhpm create customer, group, customergroup

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -2,6 +2,10 @@ import { Account } from './basic-models/account.model';
 import { Role } from './basic-models/role.model';
 import { AccountRole } from './basic-models/account-role.model';
 
+import { Customer } from './basic-models/customer.model';
+import { Group } from './basic-models/group.model';
+import { CustomerGroup } from './basic-models/customer-group';
+
 import { WorkFlow } from './bpmn-models/work-flow.model';
 import { WorkFlowInstance } from './bpmn-models/work-flow-instance.model';
 import { WorkFlowConnection } from './bpmn-models/work-flow-connection.model';
@@ -10,9 +14,13 @@ export * from './basic-models/account.model';
 export * from './basic-models/role.model';
 export * from './basic-models/account-role.model';
 
+export * from './basic-models/customer.model';
+export * from './basic-models/group.model';
+export * from './basic-models/customer-group';
+
 export * from './bpmn-models/work-flow.model';
 export * from './bpmn-models/work-flow-instance.model';
 export * from './bpmn-models/work-flow-connection.model';
 
-export const BASIC_MODELS = [Account, Role, AccountRole];
+export const BASIC_MODELS = [Account, Role, AccountRole, Customer, Group, CustomerGroup];
 export const BPMN_MODELS = [WorkFlow, WorkFlowInstance, WorkFlowConnection];
