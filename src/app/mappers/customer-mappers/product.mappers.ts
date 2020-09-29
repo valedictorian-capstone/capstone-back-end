1a86c6a79bf5918e32a341c7be0c346e6598bc6b
minhthanh1703 <minhthanh1703>
2020-09-29 22:14:41 +0700

    thanhpm: fix update api for formgroup

--- src/app/mappers/customer-mappers/product.mappers.ts
+++ src/app/mappers/customer-mappers/product.mappers.ts
@@ -1,12 +1,13 @@
 import { ProfileBase, AutoMapper, mapWith } from "@nartc/automapper";
-import { Product } from "@models";
+import { Product, ProductExtraValue } from "@models";
 import { ProductUM, ProductVM } from "@view-models";
+import { mapFrom } from "nestjsx-automapper";
 
 export class ProductMapper extends ProfileBase {
   constructor(mapper: AutoMapper) {
     super();
     mapper.createMap(Product, ProductVM)
-      .forMember(d => d.productExtraValues, mapWith(ProductVM, s => s.productExtraValues))
+    .forMember(d => d.productExtraValues, mapFrom(s => mapper.mapArray(s.productExtraValues, ProductExtraValue, ProductExtraValue)))
     mapper.createMap(ProductUM, ProductVM);
   }
 }
