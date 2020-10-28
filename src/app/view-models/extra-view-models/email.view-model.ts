6678a78bee4f91b787137b9e868cc43b66800031
minhthanh1703 <minhthanh1703>
2020-10-29 00:14:25 +0700

    thanhpm: create function send mail manual

---
+++ src/app/view-models/extra-view-models/email.view-model.ts
@@ -0,0 +1,8 @@
+import { CustomerVM } from "../customer-view-models";
+
+export class EmailManual{
+
+    public readonly info: CustomerVM;
+    public readonly subject: string;
+    public readonly content: string
+}
