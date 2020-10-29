b5ab9b9bd4c9bd2f6c645a0cae943de7d4f2779b
vinhdk <49849883+vinhdk@users.noreply.github.com>
2020-10-29 15:25:34 +0700

    vinhdk: adapt express

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,31 @@
 import { AllExceptionsFilter } from '@extras/filters';
 import { LogRequestInterceptor } from '@extras/intercepters/log.intercepter';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { FirebaseService } from '@services';
-import { json, urlencoded, NextFunction, Request, Response } from 'express';
+import express, { urlencoded, json, Request, Response, NextFunction } from 'express';
 import { AppModule } from 'src/app/app.module';
 declare const module: any;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const open = require("open");
 (async () => {
-  const origins = ["http://localhost:8080", "https://crm-capstione-be.azurewebsites.net", "http://localhost:4200", "https://m-crm-admin.web.app", "https://m-crm-app.web.app"]
-  const app = await NestFactory.create(AppModule, {
-    logger: true, cors: true, bodyParser: true
+  const server = express();
+  server.use(urlencoded({ limit: 1024 * 1024 * 500, extended: true, type: 'application/x-www-form-urlencoding' }));
+  server.use(json({ limit: 1024 * 1024 * 500, type: 'application/json' }));
+  server.use((req: Request, res: Response, next: NextFunction) => {
+    const origins = ["http://localhost:8080", "https://m-crm-company.ts.r.appspot.com/", "https://crm-capstione-be.azurewebsites.net", "http://localhost:4200", "https://m-crm-admin.web.app", "https://m-crm-app.web.app"]
+    const origin = req.headers.origin as any;
+    if (origins.indexOf(origin) > -1) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,authorization");
+    next();
   });
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LogRequestInterceptor());
-  app.use(urlencoded({ limit: 1024 * 1024 * 500, extended: true, type: 'application/x-www-form-urlencoding' }));
-  app.use(json({ limit: 1024 * 1024 * 500, type: 'application/json' }));
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('CRM BE')
     .setDescription('ALL API OF CRM')
