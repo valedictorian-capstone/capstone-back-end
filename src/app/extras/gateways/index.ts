582b6c32b79e4957829eea2c681a1f1de114ca83
vinhdk <49849883+vinhdk@users.noreply.github.com>
2020-11-10 03:15:20 +0700

    vinhdk: add socket.io

--- src/app/extras/gateways/index.ts
+++ src/app/extras/gateways/index.ts
@@ -4,20 +4,38 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
+import { CommentService } from '@services';
+import { CommentCM } from '@view-models';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(4001, { transport: ['websocket'] })
+@WebSocketGateway()
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
-  public readonly wss;
+  public readonly server: Server;
 
   protected readonly logger = new Logger('AppGateway');
-
-  handleDisconnect(client: any) {
+  constructor(
+    protected readonly commentService: CommentService,
+  ) { }
+  handleDisconnect(client: Socket) {
+    client.emit('connection', 'Disconnect');
     this.logger.log('Client disconnected');
   }
   handleConnection(client: any, ...args: any[]) {
+    console.log(...args);
     this.logger.log('New client connected');
     client.emit('connection', 'Successfully connected to server');
+  }
+  @SubscribeMessage('comments')
+  async handleEvent(
+    @MessageBody() comment: CommentCM,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const data = await this.commentService.insert(comment);
+    this.server.emit('comments', data);
   }
 }
